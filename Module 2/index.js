4ce145f7fca790124962c8a078f0680aeb3c0f1e
Kumar Aniket <aniketsingh637@outlook.com>
2019-04-05 16:32:42 +0530

    Add files via upload

---
+++ Module 2/index.js
@@ -0,0 +1,25 @@
+// For Of loop
+/*
+ var user= [{
+ 		"name":"aniket",
+		"age":21,
+		"number":56656565},
+	{
+ 		"name":"anmol",
+		"age":21,
+		"number":65888}	]
+for(x of user){
+		console.log(x)
+	}
+	*/
+	
+// Error as For Of works only  with arrays not with objects
+	/*
+	var user= {
+ 		"name":"aniket",
+		"age":21,
+		"number":56656565}
+for(x in user){
+		console.log(user[x])
+	}
+	*/
